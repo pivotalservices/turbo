2b6f0a0dd41ca44db579d95a9270056d66f623ee
Jérôme Wiedemann <jwiedemann@pivotal.io>
2018-03-13 00:16:06 +0100

    local files for ssh no more needed

--- terraform/common/sshkeys.tf
+++ terraform/common/sshkeys.tf
@@ -3,19 +3,13 @@ resource "tls_private_key" "jumpbox_ssh_private_key" {
   rsa_bits  = "4096"
 }
 
-resource "local_file" "jumpbox_ssh_private_key_file" {
-  content  = "${tls_private_key.jumpbox_ssh_private_key.private_key_pem}"
-  filename = "./local/${terraform.workspace}/ssh/jumpbox"
-}
-
-resource "local_file" "jumpbox_ssh_public_key_file" {
-  content  = "${tls_private_key.jumpbox_ssh_private_key.public_key_openssh}"
-  filename = "./local/${terraform.workspace}/ssh/jumpbox.pub"
-}
-
 output "jumpbox_ssh_private_key" {
   sensitive = true
   value     = "${tls_private_key.jumpbox_ssh_private_key.private_key_pem}"
+}
+
+output "jumpbox_ssh_public_key" {
+  value = "${tls_private_key.jumpbox_ssh_private_key.public_key_openssh}"
 }
 
 output "jumpbox_ssh_user" {
