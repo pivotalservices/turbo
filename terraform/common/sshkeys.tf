a88b62a827baf0a3dd0d7f8a82e446225a2e1307
Jérôme Wiedemann <jwiedemann@pivotal.io>
2018-03-13 00:47:53 +0100

    ssh_public_key output as sensitive

--- terraform/common/sshkeys.tf
+++ terraform/common/sshkeys.tf
@@ -9,7 +9,8 @@ output "jumpbox_ssh_private_key" {
 }
 
 output "jumpbox_ssh_public_key" {
-  value = "${tls_private_key.jumpbox_ssh_private_key.public_key_openssh}"
+  sensitive = true
+  value     = "${tls_private_key.jumpbox_ssh_private_key.public_key_openssh}"
 }
 
 output "jumpbox_ssh_user" {
