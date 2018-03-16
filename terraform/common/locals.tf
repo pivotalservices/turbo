021916149d2d398f7b8909a3da70cecae497c8ec
Jérôme Wiedemann <jwiedemann@pivotal.io>
2018-03-16 12:09:30 +0100

    typo in locals

--- terraform/common/locals.tf
+++ terraform/common/locals.tf
@@ -30,7 +30,7 @@ locals {
 
   common_flags = {
     metrics                   = "${var.deploy_metrics}"
-    compiled_releaes          = "true"
+    compiled_releases         = "true"
     metrics_compiled_releases = "true"
     use_external_postgres     = "false"
   }
