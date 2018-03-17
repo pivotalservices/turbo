9859a4509896b35c561a54bfa154414be8383485
Jérôme Wiedemann <jwiedemann@pivotal.io>
2018-03-17 14:11:25 +0100

    Correct pipeline issue

--- ci/compiled-releases/configure.sh
+++ ci/compiled-releases/configure.sh
@@ -6,6 +6,8 @@ fly -t env0 set-pipeline -n \
 	-p compile-releases \
 	-c pipeline.yml
 
+fly -t env0 unpause-pipeline -p compile-releases
+
 fly -t env0 check-resource -r compile-releases/concourse-release -f version:1.0.0
 fly -t env0 check-resource -r compile-releases/credhub-release -f version:1.0.0
 fly -t env0 check-resource -r compile-releases/uaa-release -f version:1
