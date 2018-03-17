14f2bcc8c9c1ee0c369b2615a9ba5180d2101bb4
Jérôme Wiedemann <jwiedemann@pivotal.io>
2018-03-17 17:22:10 +0100

    Update ci

--- ci/compiled-releases/configure.sh
+++ ci/compiled-releases/configure.sh
@@ -13,7 +13,7 @@ fly -t env0 check-resource -r compile-releases/credhub-release -f version:1.0.0
 fly -t env0 check-resource -r compile-releases/uaa-release -f version:1
 fly -t env0 check-resource -r compile-releases/postgres-release -f version:1
 fly -t env0 check-resource -r compile-releases/garden-runc-release -f version:1.0.0
-fly -t env0 check-resource -r compile-releases/bbr-sdk-release -f version:1.0.0
+fly -t env0 check-resource -r compile-releases/backup-and-restore-sdk-release -f version:1.0.0
 fly -t env0 check-resource -r compile-releases/grafana-release -f version:1
 fly -t env0 check-resource -r compile-releases/influxdb-release -f version:1
 fly -t env0 check-resource -r compile-releases/riemann-release -f version:1
---
+++ ci/compiled-releases/tasks/update-compiled-release-ops-file/task.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+git clone turbo turbo-out
+
+# Get release details
+for i in $(ls -1 *-compiled-release); do
+	pushd $i >/dev/null
+	tar -xzf *.tgz $(tar -tzf *.tgz | grep 'release.MF')
+	release_name=$(grep -E '^name: ' release.MF | awk '{print $2}' | tr -d "\"'")
+	release_version=$(grep -E '^version: ' release.MF | awk '{print $2}' | tr -d "\"'")
+	sha1=$(sha1sum *.tgz | cut -d' ' -f1)
+	url=$(sed -e 's/gs:\/\//https:\/\/storage.googleapis.com\//g' url)
+	popd >/dev/null
+
+	# Create ops-file
+	pushd turbo-out >/dev/null
+	OPS_FILE_PATH="deployments/ucc/ops/9-compiled-${release_name}-release.yml"
+	cat >"${OPS_FILE_PATH}" <<YML
+- type: replace
+  path: /releases/name=${release_name}?
+  value:
+    name: ${release_name}
+    version: ${release_version}
+    url: ${url}
+    sha1: ${sha1}
+YML
+	if [[ -z $(git config --global user.email) ]]; then
+		git config --global user.email "ci@wnetworks.org"
+	fi
+	if [[ -z $(git config --global user.name) ]]; then
+		git config --global user.name "CI Bot"
+	fi
+
+	if [[ -n $(git status -s) ]]; then
+		git add "${OPS_FILE_PATH}"
+		git commit -m "Compiled releases updated: ${release_name}/${release_version}"
+	fi
+	popd >/dev/null
+done
