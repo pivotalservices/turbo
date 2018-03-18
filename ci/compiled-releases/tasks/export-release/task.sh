d82655be6e1b703f0891fdbcd0f771e7fe545465
Jérôme Wiedemann <jwiedemann@pivotal.io>
2018-03-18 14:27:57 +0100

    Update Bosh compiled releases pipeline

--- ci/compiled-releases/tasks/export-release/task.sh
+++ ci/compiled-releases/tasks/export-release/task.sh
@@ -2,10 +2,6 @@
 
 set -eu
 
-start-bosh
-
-source /tmp/local-bosh/director/env
-
 #
 # stemcell metadata/upload
 #
@@ -14,20 +10,28 @@ tar -xzf stemcell/*.tgz $(tar -tzf stemcell/*.tgz | grep 'stemcell.MF')
 STEMCELL_OS=$(grep -E '^operating_system: ' stemcell.MF | awk '{print $2}' | tr -d "\"'")
 STEMCELL_VERSION=$(grep -E '^version: ' stemcell.MF | awk '{print $2}' | tr -d "\"'")
 
-bosh -n upload-stemcell stemcell/*.tgz
-
 #
 # release metadata/upload
 #
 
-cd release
+pushd release >/dev/null
 tar -xzf *.tgz $(tar -tzf *.tgz | grep 'release.MF')
 RELEASE_NAME=$(grep -E '^name: ' release.MF | awk '{print $2}' | tr -d "\"'")
 RELEASE_VERSION=$(grep -E '^version: ' release.MF | awk '{print $2}' | tr -d "\"'")
 
-bosh -n upload-release *.tgz
-cd ../
+popd >/dev/null
 
+releases_in_bucket=$(curl -s "https://storage.googleapis.com/bosh-compiled-release-tarballs/?prefix=${RELEASE_NAME}")
+if [[ ${releases_in_bucket} == *"${RELEASE_VERSION}-${STEMCELL_OS}-${STEMCELL_VERSION}"-* ]]; then
+	exit 0
+fi
+
+start-bosh
+source /tmp/local-bosh/director/env
+bosh -n upload-stemcell stemcell/*.tgz
+pushd release >/dev/null
+bosh -n upload-release *.tgz
+popd >/dev/null
 #
 # compilation deployment
 #
@@ -53,5 +57,5 @@ EOF
 bosh -n -d compilation deploy manifest.yml
 bosh -d compilation export-release $RELEASE_NAME/$RELEASE_VERSION $STEMCELL_OS/$STEMCELL_VERSION
 
-mv *.tgz compiled-release/
+mv *.tgz compiled-release/$(echo *.tgz | sed "s/\.tgz$/-$(date -u +%Y%m%d%H%M%S).tgz/")
 sha1sum compiled-release/*.tgz
