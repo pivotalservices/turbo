63633010a148cba36b1fd5c093833b50f0273039
Jérôme Wiedemann <jwiedemann@pivotal.io>
2018-03-20 12:20:41 +0100

    Adding director backup task

---
+++ ci/backup-restore/tasks/backup-director/task.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+echo "$JUMPBOX_SSH_KEY" >id_rsa && chmod 600 id_rsa
+
+mkdir backups >/dev/null 2>&1 || true
+
+printf -- "$BOSH_SSH_KEY" >id_rsa
+chmod 600 id_rsa
+
+pushd backups
+echo "Running pre-backup-checks on director..."
+./bbr/bbr director --private-key-path ./id_rsa \
+	--username "$BOSH_SSH_USER" \
+	--host "$BOSH_DIRECTOR_HOST" \
+	pre-backup-check
+
+echo "Backing up director '$BOSH_DIRECTOR_HOST'..."
+if ! ./bbr/bbr director --private-key-path ./id_rsa \
+	--username "$BOSH_SSH_USER" \
+	--host "$BOSH_DIRECTOR_HOST" \
+	backup; then
+	echo "Backup failed, cleaning up..."
+	./bbr/bbr director --private-key-path ./id_rsa \
+		--username "$BOSH_SSH_USER" \
+		--host "$BOSH_DIRECTOR_HOST" \
+		backup-cleanup
+fi
+
+tar -cvf director-backup-$(date -u +%Y%m%d%H%M%S).tar -- *
--- ci/backup-restore/tasks/backup-jumpbox/task.sh
+++ ci/backup-restore/tasks/backup-jumpbox/task.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 
 echo "$JUMPBOX_SSH_KEY" >id_rsa && chmod 600 id_rsa
 
-mkdir backups || true
+mkdir backups >/dev/null 2>&1 || true
+echo "Backing up jumpbox: '${JUMPBOX_SSH_USER}"@"${JUMPBOX_HOST}:${BOSH_STATE_FOLDER}'"
 ssh "${JUMPBOX_SSH_USER}"@"${JUMPBOX_HOST}" \
 	-i ./id_rsa \
 	-o "IdentitiesOnly=true" \
