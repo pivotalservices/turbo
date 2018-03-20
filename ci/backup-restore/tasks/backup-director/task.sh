a5b6e1b18483bab7f023eb990d502e4397e96ef6
Jérôme Wiedemann <jwiedemann@pivotal.io>
2018-03-20 14:14:37 +0100

    Fix paths for id_rsa key in backup-director task

--- ci/backup-restore/tasks/backup-director/task.sh
+++ ci/backup-restore/tasks/backup-director/task.sh
@@ -9,18 +9,18 @@ chmod 600 id_rsa
 
 pushd backups
 echo "Running pre-backup-checks on director..."
-../bbr/bbr director --private-key-path ./id_rsa \
+../bbr/bbr director --private-key-path ../id_rsa \
 	--username "$BOSH_SSH_USER" \
 	--host "$BOSH_DIRECTOR_HOST" \
 	pre-backup-check
 
 echo "Backing up director '$BOSH_DIRECTOR_HOST'..."
-if ! ../bbr/bbr director --private-key-path ./id_rsa \
+if ! ../bbr/bbr director --private-key-path ../id_rsa \
 	--username "$BOSH_SSH_USER" \
 	--host "$BOSH_DIRECTOR_HOST" \
 	backup; then
 	echo "Backup failed, cleaning up..."
-	../bbr/bbr director --private-key-path ./id_rsa \
+	../bbr/bbr director --private-key-path ../id_rsa \
 		--username "$BOSH_SSH_USER" \
 		--host "$BOSH_DIRECTOR_HOST" \
 		backup-cleanup
