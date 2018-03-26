07c5d1baeb7a323301e8e63a01418ca7fd52ebac
Jérôme Wiedemann <jwiedemann@pivotal.io>
2018-03-26 15:17:56 +0200

    Azure: Ignore TTL modification on NS entries

--- terraform/azure/dns.tf
+++ terraform/azure/dns.tf
@@ -7,32 +7,32 @@ resource "azurerm_dns_zone" "turbo" {
   }
 }
 
-resource "azurerm_dns_ns_record" "turbo" {
-  name                = "@"
-  zone_name           = "${azurerm_dns_zone.turbo.name}"
-  resource_group_name = "${azurerm_resource_group.turbo.name}"
-  ttl                 = 300
+# resource "azurerm_dns_ns_record" "turbo" {
+#   name                = "@"
+#   zone_name           = "${azurerm_dns_zone.turbo.name}"
+#   resource_group_name = "${azurerm_resource_group.turbo.name}"
+#   ttl                 = 300
 
-  record {
-    nsdname = "${azurerm_dns_zone.turbo.name_servers[0]}"
-  }
+#   record {
+#     nsdname = "${azurerm_dns_zone.turbo.name_servers[0]}"
+#   }
 
-  record {
-    nsdname = "${azurerm_dns_zone.turbo.name_servers[1]}"
-  }
+#   record {
+#     nsdname = "${azurerm_dns_zone.turbo.name_servers[1]}"
+#   }
 
-  record {
-    nsdname = "${azurerm_dns_zone.turbo.name_servers[2]}"
-  }
+#   record {
+#     nsdname = "${azurerm_dns_zone.turbo.name_servers[2]}"
+#   }
 
-  record {
-    nsdname = "${azurerm_dns_zone.turbo.name_servers[3]}"
-  }
+#   record {
+#     nsdname = "${azurerm_dns_zone.turbo.name_servers[3]}"
+#   }
 
-  tags {
-    turbo = "${var.env_name}"
-  }
-}
+#   tags {
+#     turbo = "${var.env_name}"
+#   }
+# }
 
 resource "azurerm_dns_ns_record" "turbo_master" {
   name                = "${replace(var.dns_domain_name, format(".%s", var.master_dns_domain_name), "")}"
