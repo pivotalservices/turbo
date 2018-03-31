ecf30bd3d0611ef17497b1c8b145cfc840a9a2c4
Jérôme Wiedemann <jwiedemann@pivotal.io>
2018-03-31 14:19:02 +0200

    Pipeline update

--- ci/devel-testing/configure.sh
+++ ci/devel-testing/configure.sh
@@ -4,8 +4,13 @@ set -euo pipefail
 
 # credhub set -n /concourse/main/ssh_key_gitlab -t value -v "$(lpass show --notes "id_rsa_gitlab")" > /dev/null
 # credhub set -n /concourse/main/gcp_key -t password -w "$(lpass show --notes 'turbo-140')" > /dev/null
-# credhub set -n /concourse/main/aws_access_key -t password -w ....
-# credhub set -n /concourse/main/aws_secret_key -t password -w ....
+# credhub set -n /concourse/main/aws_access_key -t password -w $(lpass show --notes 'AWS - FE-jwiedemann' | grep aws_access_key | cut -d ":" -f 2)
+# credhub set -n /concourse/main/aws_secret_key -t password -w $(lpass show --notes 'AWS - FE-jwiedemann' | grep aws_secret_key | cut -d ":" -f 2)
+# for i in $(lpass show --notes 'Azure - turbo'); do
+# 	var="arm_$(echo $i | cut -d ':' -f 1)"
+# 	value="$(echo $i | cut -d ':' -f 2)"
+# 	credhub set -n /concourse/main/$var -t value -v "$value"
+# done
 
 repo_root=$(git rev-parse --show-toplevel)
 pipeline="turbo-testing"
